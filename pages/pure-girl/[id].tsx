8c96cd63e35cc369f499923b3feadd9e5370d0d0
jesse <szx961102@gmail.com>
2021-08-30 15:04:51 +0800

    update

--- pages/pure-girl/[id].tsx
+++ pages/pure-girl/[id].tsx
@@ -8,7 +8,7 @@ import {getPicDetail, getPureGirls} from "../../api";
 import {GetStaticProps} from "next";
 
 export async function getStaticPaths() {
-    let data: PageResp<Pic> = await getPureGirls({current: 1, size: 10000})
+    let data: PageResp<Pic> = await getPureGirls({current: 1, size: 500})
     let paths = data.records.map(i => ({params: {id: i.id.toString()}}))
     return {
         paths,
