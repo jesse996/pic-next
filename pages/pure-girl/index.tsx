7c5ff246b6e8e74ad06cc240639a670d3e40d108
jesse <szx961102@gmail.com>
2021-09-02 17:02:37 +0800

    update

--- pages/pure-girl/index.tsx
+++ pages/pure-girl/index.tsx
@@ -1,20 +1,20 @@
-import {getCosplay, getNewsList, getPureGirls} from '../../api'
+import {getPureGirls} from '../../api'
 import PicList from '../../components/PicList'
-import MyLayout from "../../components/MyLayout";
 import {GetStaticProps, InferGetStaticPropsType} from "next";
-import {NewsResp, PageResp} from "../../types";
+import {PageResp, Pic} from "../../types";
 
 export const getStaticProps: GetStaticProps = async () => {
-    let data: PageResp<NewsResp> = await getNewsList({current: 1, size: 50})
+    let data: PageResp<Pic> = await getPureGirls({current: 1, size: 50})
     return {
         props: {
-            data: data.records
+            data: data.records,
+            total:data.total
         }
     }
 }
 
 export default function PureGirl({initData, total}: InferGetStaticPropsType<typeof getStaticProps>) {
     return <>
-            <PicList getList={getPureGirls} path="pure-girl" initData={initData} total={total}/>
+        <PicList getList={getPureGirls} path="pure-girl" initData={initData} total={total}/>
     </>
 }
