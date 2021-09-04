94474112ee2c49bd4093b75ef13f89be61bf1835
jesse <szx961102@gmail.com>
2021-09-04 23:04:48 +0800

    sign baidu tong ji

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>妹子图 绅士社</title>
+                    <script dangerouslySetInnerHTML={{
+                        __html: ` var _hmt = _hmt || [];
+                        (function() {
+                        var hm = document.createElement("script");
+                        hm.src = "https://hm.baidu.com/hm.js?2f2306fdd3f4cd29b18ab3d839ae3e0c";
+                        var s = document.getElementsByTagName("script")[0];
+                        s.parentNode.insertBefore(hm, s);
+                    })();`
+                    }}>
+                    </script>
+
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
