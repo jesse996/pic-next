55cd8817ef7b25a99576e9a96134c7e633392137
jesse <szx961102@gmail.com>
2021-09-08 09:47:56 +0800

    fix document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,20 @@ class MyDocument extends Document {
                     })();`
                     }}>
                     </script>
-
+                    <meta
+                        name="keywords"
+                        content="妹子图,美女图片专题,美女图片大全,绅士,绅士社,美女,性感,清纯美女,日本美女"
+                    />
+                    <meta
+                        name="description"
+                        content="绅士,绅士社,妹子图美女专题栏目，为您精心准备各种美女图片专题，包括名站美女写真，各类美女图片，美女大全等专题。"
+                    />
+                    <meta name="baidu-site-verification" content="code-OD1Ep4Bm7W" />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1377485396123371"
+                        crossOrigin="anonymous"
+                    />
                 </Head>
                 <body>
                 <Main/>
