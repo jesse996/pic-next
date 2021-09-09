91ddb7529376b97516fc244b9000140331b3520d
jesse <szx961102@gmail.com>
2021-09-09 21:09:37 +0800

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="shortcut icon" href="/favicon.svg" />
+                    <link rel="shortcut icon" href="/favicon.svg"/>
                     <script dangerouslySetInnerHTML={{
                         __html: ` var _hmt = _hmt || [];
                         (function() {
@@ -21,6 +21,15 @@ class MyDocument extends Document {
                     })();`
                     }}>
                     </script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `var _hmt = _hmt || [];
+                        (function() {
+                          var hm = document.createElement("script");
+                          hm.src = "https://hm.baidu.com/hm.js?ba42bc5adec34305840039f52547a8dc";
+                          var s = document.getElementsByTagName("script")[0]; 
+                          s.parentNode.insertBefore(hm, s);
+                        })();`
+                    }}/>
                     <meta
                         name="keywords"
                         content="妹子图,美女图片专题,美女图片大全,绅士,绅士社,美女,性感,清纯美女,日本美女"
@@ -29,7 +38,7 @@ class MyDocument extends Document {
                         name="description"
                         content="绅士,绅士社,妹子图美女专题栏目，为您精心准备各种美女图片专题，包括名站美女写真，各类美女图片，美女大全等专题。"
                     />
-                    <meta name="baidu-site-verification" content="code-OD1Ep4Bm7W" />
+                    <meta name="baidu-site-verification" content="code-OD1Ep4Bm7W"/>
                     <script
                         async
                         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1377485396123371"
